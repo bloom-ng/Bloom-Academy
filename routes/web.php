3a60c0c65c2b44496f655e1abc20b9489f138cce
bloom-ng <it@bloomdigitmedia.com>
2024-07-12 01:15:25 +0000

    added webhook url

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserDataController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -38,3 +39,32 @@ Route::get('/submit', function () {
 });
 
 Route::post('/process-payment', [UserDataController::class, 'store']);
+
+//Webhook - Flutterwave
+Route::post('/flw-webhook', function (Request $request) {
+    // If you specified a secret hash, check for the signature
+    $secretHash = env('FLUTTERWAVE_SECRET_HASH');
+    $signature = $request->header('verif-hash');
+
+    if (!$signature || ($signature !== $secretHash)) {
+        // This request isn't from Flutterwave; discard
+        abort(401);
+    }
+
+    $payload = $request->all();
+    // It's a good idea to log all received events.
+    Log::info($payload);
+
+    // Ensure the storage directory exists
+    $directory = storage_path('flutterwave_payloads');
+    if (!is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+
+    // Write the payload to a file
+    $filename = $directory . '/flutterwave_payload_' . date('YmdHis') . '.txt';
+    file_put_contents($filename, json_encode($payload, JSON_PRETTY_PRINT));
+
+    // Do something (that doesn't take too long) with the payload
+    return response()->json(['status' => 'success'], 200);
+});
